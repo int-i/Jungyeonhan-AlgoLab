cba487e30025209b6861757c4d030709c1265c78
지나이 지나이 <lsj77205619@gmail.com>
2023-05-03 12:46:11 +0900

    [Homework] 백준 2566

---
+++ 이승준/2566.py
@@ -0,0 +1,14 @@
+li = [[int(i) for i in input().split()] for _ in range(9)]
+max=0
+x=0
+y=0
+for i in range(9):
+    for j in range(9):
+        if li[i][j] > max:
+            x=i
+            y=j
+            max=li[i][j]
+
+print(max)
+print(x+1,end = " ")
+print(y+1)
