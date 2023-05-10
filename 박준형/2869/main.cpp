80b9deb2c7cadf9e0c828946c34497f2fb0ef7de
준형 박 <99jhpark@naver.com>
2023-05-11 00:34:26 +0900

    [Homework] 2869수정

--- 박준형/2869/main.cpp
+++ 박준형/2869/main.cpp
@@ -1,19 +1,20 @@
-#include<stdio.h>
 #include <iostream>
+#include <cmath>
 using namespace std;
-int main() {
-	int A, B, V;
-	cin >> A >> B >> V;
-	int left = V;
-	int day = 0;
-	while (left > 0) {
-		left = left - A;
-		day++;
-		if (left <= 0) {
-			break;
-		}
-		left = left + B;
-	}
-	cout << day;
 
+int main() {
+    int A, B, V;
+    cin >> A >> B >> V;
+
+    int climbing_per_day = A - B;
+    int distance_to_cover = V - A;
+
+    // 마지막 날을 포함한 전체 일수 계산
+    int days = (int)floor((double)distance_to_cover / climbing_per_day) + 1;
+
+    if (distance_to_cover % climbing_per_day != 0) {
+        days += 1;
+    }
+
+    cout << days;
 }
