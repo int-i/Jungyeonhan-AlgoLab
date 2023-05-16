5d6119396be4ca151d71fc04e19214e899b8819a
지나이 지나이 <lsj77205619@gmail.com>
2023-05-16 19:23:02 +0900

    [Homework] 백준 17202

---
+++ 이승준/17202.py
@@ -0,0 +1,14 @@
+a=list((input()))
+b=list((input()))
+summ=[0]*17
+for i in range(8):
+    summ[2*i]=a[i]
+    summ[2*i+1]=b[i]
+for i in range(0,14):
+    for n in range(0,15-i):
+        summ[n]=int(summ[n])+int(summ[n+1])
+        summ[n]=summ[n]%10
+    summ[15-i]=0
+print(10*summ[0]+summ[1])
+
+
